abda21f564bbb922061d2add8f6b16fc45b5b0bb
Christopher Thornton <merge.tool>
2018-01-27 23:03:07 -0700

    Initialize new repo

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # seq-annot
-Tools to fascilitate annotation of genomes and metagenomes
+Tools to fascilitate annotation and comparison of genomes and metagenomes
+
+## About
+
+seq-annot is a python package for annotating and counting genomic features
+in genomes and metagenomes.
+
+## Requirements
+
+Python 3.4+
+
+Python Libraries:
+
+* arandomness
+* bio_utils
+* HTSeq
+
+## Installation
+
+pip install seq-annot
+
+## Usage
+
+screen_features
+
+### Examples
+
+    screen_features 
+
+annotate_features
+
+### Examples
+
+    annotate_features
+
+count_features
+
+## Examples
+
+    count_features
+
+combine_features
+
+## Examples
+
+    combine_features
+
+compare_features
+
+## Examples
+
+    compare_features
+    
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+from setuptools import setup
+
+setup(name='seq-annot',
+      version='0.1.0',
+      packages=['seq_annot',],
+      description='Tools that fascilitate the annotation and functional '
+          'comparison of genomes and metagenomes',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      keywords='bioinformatics sequence profiling annotation',
+      url='https://github.com/Brazelton-Lab/seq-annot/',
+      download_url = 'https://github.com/Brazelton-Lab/seq-annot/archive/v0.0.1.tar.gz',
+      author='Christopher Thornton',
+      author_email='christopher.thornton@utah.edu',
+      license='GPLv3',
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['bio_utils', 'HTSeq', 'arandomness'],
+      entry_points={
+          'console_scripts': [
+              'compare_features = seq_annot.compare:main',
+              'count_features = seq_annot.count:main',
+              'annotate_features = seq_annot.annotate:main',
+              'screen_features = seq_annot.screen:main',
+              'combine_features = seq_annot.combine:main'
+          ]
+      }
+      )
