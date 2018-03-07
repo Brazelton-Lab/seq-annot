978bbb63b4aecf83bcef1bcf598717f661ee18cf
Christopher Thornton <christopher.n.thornton@gmail.com>
2018-03-06 21:33:30 -0700

    Version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='seq-annot',
-      version='0.1.0',
+      version='0.1.8',
       packages=['seq_annot',],
       description='Tools that fascilitate the annotation and functional '
           'comparison of genomes and metagenomes',
