f523e1e806dbce601959c1a69fe2adde07cc0743
Christopher Thornton <merge.tool>
2018-07-01 21:02:35 -0600

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='seq-annot',
-      version='0.2.3',
+      version='0.2.4',
       packages=['seq_annot',],
       description='Tools that fascilitate the annotation and functional '
           'comparison of genomes and metagenomes',
