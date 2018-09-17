3d55280d9916877efa7fa0a2a06cfdd83ddd7f03
Christopher Thornton <merge.tool>
2018-09-17 02:19:48 -0600

    add ability to combine rows of an abundance table based on field values in relational database

--- seq_annot/manipulate.py
+++ seq_annot/manipulate.py
@@ -1,9 +1,12 @@
 #! /usr/bin/env python
 """
-Manipulate a feature abundance table by inserting new database fields and 
-subsetting via screening of the relational database.
+Manipulate feature abundance table. Objects can be filtered by searching a 
+relational database for specified patterns. The database can also be used to 
+merge objects based on shared field values. If desired, an accompanying 
+table of contextual information in TSV format will be created from the 
+database fields.
 
-Required input is a feature abundance table in TSV format and a relational
+Required input is a feature abundance table in TSV format and a relational 
 database in JSON format.
 
 The compression algorithm is automatically detected for input files based on
@@ -34,7 +37,7 @@ Copyright:
 from arandomness.argparse import Open, ParseSeparator
 import argparse
 import os
-from seq_annot.reldb import filter_dbs, load_dbs
+from seq_annot.reldb import *
 from seq_annot.seqio import open_io, write_io
 import sys
 import textwrap
@@ -44,7 +47,39 @@ __author__ = 'Christopher Thornton'
 __license__ = 'GPLv3'
 __maintainer__ = 'Christopher Thornton'
 __status__ = "Alpha"
-__version__ = '0.3.3'
+__version__ = '0.4.2'
+
+
+def combine_rows(r1: list, r2: list):
+    """Perform vector addition for two rows in an abundance table.
+    """
+    num_type = type(float())
+
+    if len(r1) != len(r2):
+        print("error: rows have differing numbers of columns", file=sys.stderr)
+        sys.exit(1)
+
+    try:
+        merged = list(map(sum, zip(r1, r2)))
+    except TypeError:
+        print("error: all cells must have numerical values", file=sys.stderr)
+        sys.exit(1)
+
+    return merged
+
+
+def format_entry(mapping: dict, entry_id: str, fields=None):
+    """Format relevant database entry for writing
+    """
+    try:
+        entry = mapping[entry_id]
+    except KeyError:
+        values = ["NA"] * len(fields)
+        entry_csv = "{}\t{}\n".format(entry_id, '\t'.join(values))
+    else:
+        entry_csv = "{}\t{}\n".format(entry_id, entry_as_csv(entry, fields))
+
+    return entry_csv
 
 
 def main():
@@ -64,24 +99,31 @@ def main():
         sep=',',
         help="input one or more relational databases in JSON format")
     parser.add_argument('-o', '--out',
+        metavar='out.csv',
         action=Open,
         mode='wb',
         default=sys.stdout,
         help="output modified feature abundance table [default: output to "
              "stdout]")
+    parser.add_argument('-d', '--db',
+        metavar='out.csv',
+        action=Open,
+        mode='wb',
+        help="output accompanying contextual information from relational "
+             "database. Can be used with -a/--fields to specify which fields"
+             "in the database to include in the output")
     parser.add_argument('-c', '--combine',
         metavar='FIELD',
         dest='merge_field',
-        help="combine features with matching values in database field. "
-             "Requires that the field already exist in the table or be "
-             "provided in the -i/--insert argument")
-    parser.add_argument('-i', '--insert',
+        help="combine features with shared values in the database field")
+    parser.add_argument('-a', '--fields',
         metavar='FIELD [,FIELD,...]',
         dest='input_fields',
         action=ParseSeparator,
         sep=',',
         help="comma-separated list of fields from the relational database "
-             "that should be added to the abundance table")
+             "that should be added to the accompanying table of contextual "
+             "information")
     parser.add_argument('-s', '--subset',
         metavar='FIELD:PATTERN',
         dest='subset_terms',
@@ -106,13 +148,13 @@ def main():
         help="pattern matching should be case-sensitive")
     args = parser.parse_args()
 
-    if not (args.input_fields or args.subset_terms):
-        parser.error("-m/--mapping must be used with either "
-                     "-i/--insert or -f/--filter")
+    if args.map_files and not (args.input_fields or args.subset_terms or \
+                               args.merge_field or args.filter_terms):
+        parser.error("-m/--mapping must be used with one or more of "
+                     "-c/--combine, -d/--db, -s/--subset, or -f/--filter")
 
-    # Speedup trick
-    list_type = type(list())
-    str_type = type(str())
+    if args.db and not args.input_fields:
+        parser.error("-d/--db should be used -a/--fields")
 
     # Output run information
     all_args = sys.argv[1:]
@@ -126,19 +168,22 @@ def main():
 
     # Assign variables based on user input
     out_h = args.out
+    out_db = args.db
 
     cs = True if args.match_cs else False
 
+    merge_field = args.merge_field
     add_fields = args.input_fields if args.input_fields else []
     subset_fields = [i.split(':', 1)[0] for i in args.subset_terms] \
                     if args.subset_terms else []
     filter_fields = [i.split(':', 1)[0] for i in args.filter_terms] \
                     if args.filter_terms else []
 
-    all_fields = add_fields + subset_fields + filter_fields
+    all_fields = add_fields + subset_fields + filter_fields + [merge_field]
 
     # Load databases
-    mapping = load_dbs(args.map_files, fields=all_fields)
+    mapping = load_dbs(args.map_files, fields=all_fields) if args.map_files \
+              else {}
 
     # Remove entries from database if values do not match any of the subset 
     # patterns
@@ -151,25 +196,28 @@ def main():
         mapping = filter_dbs(mapping, patterns=args.filter_terms, 
                              subset=False, case=cs)
 
-    # Insert new fields into table header, if applicable
+    # Output headers
     try:
         header = args.abunds.readline().decode('utf-8')
     except AttributeError:
         header = args.abunds.readline()
 
-    header = header.strip().split('\t')
-    header = header[0:1] + add_fields + header[1:]
+    write_io(out_h, header)
 
-    write_io(out_h, '{}\n'.format('\t'.join(header)))
+    if out_db and not merge_field:
+        db_header = "{}\t{}\n".format(header.split('\t')[0], 
+                                      "\t".join(add_fields))
+        write_io(out_db, db_header)
 
     # Iterate over feature abundance table
-    for row in args.abunds:
+    merged_data = {}
+    for line_pos, row in enumerate(args.abunds):
         try:
             row = row.decode('utf-8')
         except AttributeError:
             pass
 
-        if row.startswith('#'):  # Skip comments
+        if row.startswith('#'):  #skip comments
             continue
         
         split_row = row.strip().split('\t')
@@ -182,25 +230,50 @@ def main():
             if feature_name not in mapping:
                 continue
 
-        # Insert new field values
-        new_values = []
-        for add_field in add_fields:
+        # Output row or merge based on field value
+        if merge_field != None:
             try:
-                field_val = mapping[feature_name][add_field]
-            except KeyError:  # Feature or entry field not in database
-                field_val = 'NA'
+                field_val = mapping[feature_name][merge_field]
+            except KeyError:
+                field_val = "NA"
 
-            if type(field_val) == list_type:  # Attribute has multiple values
-                field_val = ';'.join(field_val)
+            try:
+                counts = list(map(float, split_row[1:]))
+            except ValueError:
+                print("error: found non-numeric abundance values at line {!s}"\
+                      .format(line_pos), file=sys.stderr)
+                sys.exit(1)
 
-            if not field_val:
-                field_val = 'NA'
+            try:
+                existing = merged_data[field_val]
+            except KeyError:
+                merged_data[field_val] = counts
+            else:
+                merged_data[field_val] = combine_rows(counts, existing)
+        else:
+            output = "\t".join(split_row)
+            write_io(out_h, "{}\n".format(output))
 
-            new_values.append(field_val)
- 
-        # Output row
-        output = "\t".join(split_row[0:1] + new_values + split_row[1:])
-        write_io(out_h, "{}\n".format(output))
+            # Write corresponding database entries as CSV
+            if out_db:
+                write_io(out_db, format_entry(mapping, feature_name, add_fields))
+
+    if merge_field != None:
+        if out_db:
+            mapping = derep_by_field(mapping, field=merge_field)
+            add_fields = [i for i in add_fields if i != merge_field]
+            db_header = "{}\t{}\n".format(merge_field, "\t".join(add_fields))
+            write_io(out_db, db_header)
+
+        num_type = type(float())
+
+        for new_id in merged_data:
+            output = ["{:.9g}".format(i) for i in merged_data[new_id]]
+            write_io(out_h, "{!s}\t{}\n".format(new_id, "\t".join(output)))
+
+            # Write corresponding database entries as CSV
+            if out_db:
+                write_io(out_db, format_entry(mapping, new_id, add_fields))
 
     # Calculate and print program run-time info
     end_time = time()
