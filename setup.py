50a1205c688d2991e1ab3015a09e26abc7c36396
Christopher Thornton <merge.tool>
2019-04-22 11:57:03 -0600

    Add HMM tblout format support for annotation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='seq-annot',
-      version='0.6.1',
+      version='0.7.4',
       packages=['seq_annot',],
       description='Tools that fascilitate the annotation and functional '
           'comparison of metagenomes',
