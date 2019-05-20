48449cdcaccac9bdc0b4e177dc9ace948062e0b3
Christopher Thornton <merge.tool>
2019-05-20 14:22:03 -0600

    Separate sets argument so that the second set can be made optional. If only one set is provided, look for co-occurrences within the set

--- seq_annot/colocate.py
+++ seq_annot/colocate.py
@@ -50,7 +50,7 @@ __author__ = 'Christopher Thornton'
 __license__ = 'GPLv3'
 __maintainer__ = 'Christopher Thornton'
 __status__ = "Beta"
-__version__ = '0.1.4'
+__version__ = '0.2.4'
 
 
 class GenomicRegion:
@@ -158,6 +158,35 @@ class GenomicRegion:
         else:
             return(False)
 
+    def bounded(self, f1, f2, stranded: bool=False):
+        """Determine if a feature in a genomic region is bounded by 
+
+        Args:
+            f1 (str): name of feature in genomic region
+
+            f2 (str): name of different feature in genomic region
+
+            stranded (bool): a feature can only be bounded by others if on 
+                different strands  [default: False]
+
+        Returns:
+            bool: True if features overlap, else False
+        """
+
+        pass
+
+#        try:
+#            f1_strand, f1_start, f1_end = self.cargo[f1]
+#            f2_strand, f2_start, f2_end = self.cargo[f2]
+#        except KeyError:
+#            raise
+
+        # Features can't be considered overlapping if on different strands
+#        if stranded and ((f1_strand == '+' and f2_strand == '-') or \
+#            (f1_strand == '-' and f2_strand == '+')):
+#            return(False)
+
+
     def parse_gff(self, entry, feature):
         """Populate dictionary of cargo features by parsing GFF3Entry objects
 
@@ -187,93 +216,197 @@ class GenomicRegion:
         return(ids)
 
 
-def output_dist(handle, contig, int1, int2, outall=False):
-    """Write contig length and relative distance distributions to file
+def output_dist(handle, giv, set1, set2, outall=False):
+    """Write genomic region length and relative distance distributions to file
+
+    Args:
+        handle (file): file handle of output distributions file
+
+        giv (class): GenomicRegion object containing co-occurring features
+
+        set1 (list): list of elements within set1
+
+        set2 (list): list of elements within set2
+
+        outall (bool): output distributions for all genomic regions in GFF, not
+            just for regions containing set elements [default: False]
+
+    Return:
+        exit code (int): returns 0 if able to write else 1
     """
-    length = contig.length
-    seqid = contig.seqid
-    ncargo = len(contig.cargo)
+    length = giv.length
+    seqid = giv.seqid
+    cargo = giv.cargo.keys()
+    ncargo = len(cargo)
 
-    # Contig contains elements from both sets
-    if int1 and int2:
-        idents2 = []
-        for ident in int2:
-            idents2 += contig.positional_ids(ident)
+    # Return 1 if unable to write to distributions file
+    if not seqid:
+        return(1)
 
-        for name in int1:
-            # Find shortest distance between primary feature and any feature
-            # in the secondary set
-            dists = []
-            idents1 = contig.positional_ids(name)
-            for ident in idents1:
-                dists.append(min([contig.distance(ident, k)[1] for k in \
-                    idents2]))
+    # Populate lists of features found in either or both sets
+    idents1 = []
+    idents2 = []
+    for feature_id in cargo:
+        feature = feature_id.split("_", 1)[0]
+        if feature in set1:
+            idents1.append(feature_id)
+        if feature in set2:
+            idents2.append(feature_id)
 
-            distance = min(dists)
+    if idents1 and idents2:  #contains elements from both sets
+        used_ids = []
+        for el1 in idents1:
+            name1, id1 = el1.split("_", 1)
+            for el2 in idents2:
+                if el1 == el2:  #happens when sets intersection non-empty
+                    continue
 
-            # Output contig length and distance distributions
-            names2 = ";".join(sorted(int2))
-            output = "{}\t{}\t{}\t{}\t{}\t{}\n".format(name, names2, seqid, \
-                length, distance, ncargo)
+                name2, id2 = el2.split("_", 1)
+                if id2 in used_ids:  #happens when sets intersection non-empty
+                    continue
+
+                # Find physical distance between elements
+                strand, distance = giv.distance(el1, el2)
+
+                # Output information for given instance of co-occurrence
+                output = "{}\t{}\t{}\t{!s}\t{!s}\t{!s}\n".format(name1, \
+                    name2, seqid, length, distance, ncargo)
+                write_io(handle, output)
+
+            used_ids.append(id1)
+
+    elif idents1 and not idents2:  #contains elements only from first set
+        for el1 in idents1:
+            name = el1.split("_", 1)[0]
+            output = "{}\tNA\t{}\t{!s}\tNA\t{!s}\n".format(name, seqid, \
+                length, ncargo)
             write_io(handle, output)
 
-    # Contig contains elements only from primary set
-    elif int1 and not int2:
-        for name in int1:
-            output = "{}\tNA\t{}\t{}\tNA\t{}\n".format(name, seqid, length, \
-                ncargo)
-            write_io(handle, output)
-
-    # Contig contains elements only from secondary set
-    elif int2 and not int1:
-        for name in int2:
-            output = "NA\t{}\t{}\t{}\tNA\t{}\n".format(name, seqid, length, \
-                ncargo)
+    elif idents2 and not idents1:  #contains elements only from secondary set
+        for el2 in idents2:
+            name = el2.split("_", 1)[0]
+            output = "NA\t{}\t{}\t{!s}\tNA\t{!s}\n".format(name, seqid, \
+                length, ncargo)
             write_io(handle, output)
 
     else:
         if outall:
-            output = "NA\tNA\t{}\t{}\tNA\t{}\n".format(seqid, length, ncargo)
+            output = "NA\tNA\t{}\t{!s}\tNA\t{!s}\n".format(seqid, length, \
+                ncargo)
             write_io(handle, output)
 
-def increment_occurrence(co_res, contig, set1, set2, dist=None, outall=False):
-    """Increment elements of the co-occurrence matrix when evidence of 
-    co-redidence
+    return(0)  #writes successful
+
+def increment_occurrence(occur, giv, set1, set2):
+    """Find instances of co-occurrence between set elements
+
+    Args:
+        occur (Array): numpy Array containing incidences of co-occurrence
+
+        giv (class): GenomicRegion object containing co-occurring features
+
+        set1 (list): list of elements within set1
+
+        set2 (list): list of elements within set2
+
+    Returns:
+        occur (Array): numpy Array incremented by the number instances of 
+            co-occurrence
     """
+    
     # Convert feature positional IDs back to standard name
-    features = set([i.rsplit("_", 1)[0] for i in contig.cargo.keys()])
+    features = [i.rsplit("_", 1)[0] for i in giv.cargo.keys()]
 
-    # Find intersection between elements in the sets and features on the contig
-    int1 = features.intersection(set1)
-    int2 = features.intersection(set2)
+    # Find intersection between elements in set1 and features on the region
+    int1 = set(features).intersection(set1)
 
-    if dist and contig.seqid:
-        output_dist(dist, contig, int1, int2, outall)
+    # Match intersection results to appropriate locations in co-occurrence table
+    row_pos = []
+    for i in int1:
+        row_pos.append(set1.index(i))
+        features.remove(i)
 
-    # Match intersection results to appropriate locations in occurrence table
-    row_pos = [set1.index(i) for i in int1] if int1 else [-1]
-    col_pos = [set2.index(j) for j in int2] if int2 else [-1]
+    # Find intersection between elements in set2 and remaining features
+    int2 = set(features).intersection(set2)
+    col_pos = [set2.index(j) for j in int2] if int2 else []
 
     # Increment co-occurrence table by row and column coordinates
     for r in row_pos:
         for c in col_pos:
-            co_res[r][c] += 1
+            occur[r][c] += 1
 
-    return(co_res)
+    return(occur)
+
+def parse_sets(arg_in):
+    """Parse input to sets arguments. If input is a file, read in elements of 
+    the set line-by-line, otherwise assume set elements are separated by a 
+    comma.
+    """
+    in_set = []
+
+    try:
+        with open_io(arg_in, mode='rb') as in_h:
+            for nline, row in enumerate(in_h):
+                row = row.decode('utf-8')
+
+                if row.startswith('#'):  #comments ignored
+                    continue
+
+                row = row.rstrip().split('\t')
+
+                el = row[0]
+                in_set.append(el)
+
+    except FileNotFoundError:
+        try:
+            in_set = [i.lstrip() for i in arg_in.split(",")]
+        except ValueError:
+            raise ValueError("input provided to -s/--set or -s2/--set2 should "
+                "either be a file or a comma-spearated string of set elements"\
+                .format(arg_in))
+
+    except ValueError:
+        raise FormatError("{}: line {}. Incorrect number of columns provided. "
+            "Please verify that the input file is correctly formatted"\
+            .format(csv, nline))
+
+    if not in_set:
+        raise FormatError("something went wrong while attempting to parse "
+            "input {}".format(arg_in))
+
+    return(in_set)
+
+def parse_set_entry():
+    """
+    """
+    return(entry)
+
+def do_nothing(*args, **kargs):
+    return(1)
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('in_sets',
-        metavar='in.csv',
-        help="input feature sets file in CSV format. The sets file should "
-             "contain exactly two columns of genomic feature names. Lines "
-             "starting with '#' will be ignored. Use '-' to indicate that "
-             "input should be taken from standard input (stdin)")
     parser.add_argument('in_gff',
         metavar='in.gff3',
         help="input feature annotation file in GFF3 format. Use '-' to indicate "
              "that input should be taken from standard input (stdin)")
+    parser.add_argument('-s', '--set',
+        required=True,
+        dest='in_set1',
+        metavar='in.csv',
+        help="input primary feature sets file in CSV format. The file should "
+             "contain a line-separated list of genomic feature names. Lines "
+             "starting with '#' will be ignored. Use '-' to indicate that "
+             "input should be taken from standard input (stdin)")
+    parser.add_argument('-s2', '--set2',
+        dest='in_set2',
+        metavar='in.csv',
+        help="input optional supplementary feature sets file in CSV format. "
+             "The file should contain a line-separated list of genomic feature "
+             "names. Lines starting starting with '#' will be ignored. If "
+             "unused, co-occurrences will be found between elements within the "
+             "primary set")
     parser.add_argument('-f', '--fasta',
         dest='in_fasta',
         metavar='in.fa',
@@ -315,7 +448,7 @@ def main():
         parser.error("error: argument -d/--out-dist must be supplied along "
             "with -f/--fasta")
 
-    if args.in_sets == '-' and args.in_gff == '-':
+    if args.in_set1 == '-' and args.in_gff == '-':
         parser.error("error: standard input (stdin) can only be redirected to "
             "a single positional argument")
 
@@ -341,10 +474,14 @@ def main():
     else:
         in_gff = args.in_gff
 
-    if args.in_sets == '-':
-        in_sets = sys.stdin
+    if args.in_set1 == '-':
+        in_set1 = sys.stdin
     else:
-        in_sets = args.in_sets
+        in_set1 = args.in_set1
+
+    in_set2 = args.in_set2
+
+    distribution = output_dist if out_d else do_nothing
 
     # Calculate and store contig lengths
     len_dist = {}
@@ -353,35 +490,12 @@ def main():
             len_dist[record.id] = len(record.sequence)
 
     # Populate sets with elements from the CSV file
-    set1 = []
-    set2 = []
-    with open_io(in_sets, mode='rb') as in_h:
-        for nline, row in enumerate(in_h):
-            row = row.decode('utf-8')
-
-            if row.startswith('#'):  #comments are ignored
-                continue
-
-            row = row.rstrip().split('\t')
-
-            try:
-                el1, el2 = row
-            except ValueError:
-                if len(row) == 1:  #no el2 if length set1 > length set2
-                    el1 = row[0]
-                    set1.append(el1)
-                else:  #too many columns provided
-                    raise FormatError("{}: line {}. Incorrect number of "
-                        "columns provided. Please verify file format"\
-                        .format(in_sets, nline))
-            else:
-                if el1:  #could be blank if length set2 > length set1
-                    set1.append(el1)
-                set2.append(el2)
+    set1 = parse_sets(in_set1)
+    set2 = parse_sets(in_set2) if in_set2 else set1
 
     # Prepare data array for storing occurrences of co-residence
-    rows = set1 + ["None"]
-    columns = set2 + ["None"]
+    rows = set1
+    columns = set2
 
     nrow = len(rows)
     ncol = len(columns)
@@ -415,9 +529,12 @@ def main():
         seqid = entry.seqid
         old_seqid = giv.seqid
         if seqid != old_seqid:  #new contig encountered
+
             # Process previous genomic region
-            co_res = increment_occurrence(co_res, giv, set1, set2, out_d, \
-                all_len) 
+            co_res = increment_occurrence(co_res, giv, set1, set2) 
+
+            # Output distributions
+            ecode = distribution(out_d, giv, set1, set2, all_len)
 
             # Reset for new genomic region interval
             giv = GenomicRegion()
@@ -436,7 +553,9 @@ def main():
         giv.parse_gff(entry, feature)
 
     # Process last contig
-    co_res = increment_occurrence(co_res, giv, set1, set2, out_d, all_len)
+    co_res = increment_occurrence(co_res, giv, set1, set2)
+
+    ecode = distribution(out_d, giv, set1, set2, all_len)
 
     # Output co-occurrence table
     write_io(out_h, "ID\t{}\n".format("\t".join(columns)))
